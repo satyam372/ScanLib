8aa3c1ca7171484526f9327c218aee2369336f87
satyam_45 <satyampokharna45@gmail.com>
2024-07-15 21:19:13 +0530

    Tests:Add tests for Database.dart file
    Needs Improvement.
    For now The database & operations was tested using the UI script

---
+++ test/model/database_test.dart
@@ -0,0 +1,26 @@
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:library_qr/model/database.dart';
+
+void main() {
+  late AppDb db;
+
+  setUp(() {
+    db = AppDb(NativeDatabase.memory()); // Use an in-memory database for testing
+  });
+
+  tearDown(() async {
+    await db.close();
+  });
+
+  test('Insert student data', () async {
+    // TODO: Complete this Tests
+    // ignore: unused_local_variable
+    final student = StudentsCompanion(
+      rollno: const Value('MITU21BTCS0550'),
+      name: const Value('Satyam Pokharna'),
+      intime: Value(DateTime.now()),
+    );
+  });
+}
