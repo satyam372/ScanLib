538379c60ef569d5be5ea288055410823b89fc73
satyam_45 <satyampokharna45@gmail.com>
2024-07-17 21:20:50 +0530

    scannerview:Integrating LocalApi with scannerview.dart
    Fixes issue#3

--- lib/views/scannerview.dart
+++ lib/views/scannerview.dart
@@ -1,34 +1,49 @@
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:library_qr/Repositry/student_repositry.dart';
 
-class Scannscreen extends StatefulWidget {
-  const Scannscreen({super.key});
-
+class ScanScreen extends StatefulWidget {
+  const ScanScreen({super.key});
   @override
-  // ignore: library_private_types_in_public_api
-  _ScannerScreenState createState() => _ScannerScreenState();
+  ScanScreenState createState() => ScanScreenState();
 }
 
-class _ScannerScreenState extends State<Scannscreen> {
+class ScanScreenState extends State<ScanScreen> {
   String _scanResult = 'Scan a barcode';
+  late LocalApi api;
+  @override
+  void initState() {
+    super.initState();
+    api = LocalApi();
+    api.initState();
+  }
+
+  @override
+  void dispose() {
+    api.dispose();
+    super.dispose();
+  }
 
   Future<void> _scanBarcode() async {
     String barcodeScanRes;
     try {
       barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
           '#ff6666', 'Cancel', true, ScanMode.BARCODE);
-
     } catch (e) {
       barcodeScanRes = 'Failed to get platform version.';
     }
     if (!mounted) return;
-
     setState(() {
       _scanResult = barcodeScanRes;
     });
   }
 
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,8 +63,6 @@ class _ScannerScreenState extends State<Scannscreen> {
             ElevatedButton(
               onPressed: _scanBarcode,
               child: const Text('Scan Barcode'),
-            ),
-          ],
-        )));
+            )])));
   }
 }
