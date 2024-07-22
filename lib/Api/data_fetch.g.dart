f3de02b698b5a146067d4ca67c2cf1ebcb7b33c0
satyam_45 <satyampokharna45@gmail.com>
2024-07-22 23:52:58 +0530

    Get:Add get request to fetch the info

---
+++ lib/Api/data_fetch.g.dart
@@ -0,0 +1,97 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'data_fetch.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Post _$PostFromJson(Map<String, dynamic> json) => Post(
+      rollno: (json['rollno'] as num).toInt(),
+      name: json['name'] as String,
+      department: json['department'] as String,
+    );
+
+Map<String, dynamic> _$PostToJson(Post instance) => <String, dynamic>{
+      'rollno': instance.rollno,
+      'name': instance.name,
+      'department': instance.department,
+    };
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers,unused_element
+
+class _FetchClient implements FetchClient {
+  _FetchClient(
+    this._dio, {
+    this.baseUrl,
+  }) {
+    baseUrl ??= 'http:// 192.168.19.224:8080/library';
+  }
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<List<Post>> getTasks(String rollno) async {
+    final _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{r'rollno': rollno};
+    final _headers = <String, dynamic>{};
+    const Map<String, dynamic>? _data = null;
+    final _result =
+        await _dio.fetch<List<dynamic>>(_setStreamType<List<Post>>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              'fetch_info.php',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    var _value = _result.data!
+        .map((dynamic i) => Post.fromJson(i as Map<String, dynamic>))
+        .toList();
+    return _value;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}
