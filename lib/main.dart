6e1b81fb22373cff7e40357b0ce375b2a5c8c7a9
satyam_45 <satyampokharna45@gmail.com>
2024-08-05 00:54:02 +0530

    Analyze:Add lines to analyze app's performance

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:library_qr/views/scannerview.dart';
 
 void main() {
   runApp(const MyApp());
+  WidgetsApp.debugAllowBannerOverride = true;
+  debugPaintSizeEnabled = false;
+  debugRepaintRainbowEnabled = false;
+  debugRepaintTextRainbowEnabled = false;
+  debugProfilePaintsEnabled = true;
+  debugProfileBuildsEnabled = true;
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
