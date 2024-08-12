23168dde17490101e0567ba557089a28c87f1426
satyam_45 <satyampokharna45@gmail.com>
2024-08-12 09:35:35 +0530

    database: Create table StudentArchive Fixes #38

--- lib/model/database.dart
+++ lib/model/database.dart
@@ -15,14 +15,22 @@ class Students extends Table {
   BlobColumn get signature => blob().nullable()();
 }
 
-@DriftDatabase(tables: [Students], daos: [StudentDao])
+class ArchiveStudents extends Table { // Changed to ArchiveStudents
+  TextColumn get rollno => text()();
+  TextColumn get name => text()();
+  DateTimeColumn get intime => dateTime().nullable()();
+  DateTimeColumn get outtime => dateTime().nullable()();
+  TextColumn get department => text()();
+}
+
+@DriftDatabase(tables: [Students, ArchiveStudents], daos: [StudentDao, ArchiveStudentDao]) // Included both tables
 class AppDb extends _$AppDb {
   AppDb._internal() : super(_openConnection());
 
   static final AppDb instance = AppDb._internal();
 
   @override
-  int get schemaVersion => 2;
+  int get schemaVersion => 3; // Updated to version 3
 
   static LazyDatabase _openConnection() {
     return LazyDatabase(() async {
@@ -44,6 +52,9 @@ class AppDb extends _$AppDb {
       if (from < 3) {
         await m.addColumn(students, students.signature);
       }
+      if (from < 3) { // Ensure consistency in adding new columns
+        await m.createTable(archiveStudents);
+      }
     },
   );
 }
@@ -51,7 +62,6 @@ class AppDb extends _$AppDb {
 @DriftAccessor(tables: [Students])
 class StudentDao extends DatabaseAccessor<AppDb> with _$StudentDaoMixin {
   final AppDb db;
-
   StudentDao(this.db) : super(db);
 
   Future<int> insertStudent(StudentsCompanion student) => into(students).insert(student);
@@ -65,19 +75,30 @@ class StudentDao extends DatabaseAccessor<AppDb> with _$StudentDaoMixin {
             .write(StudentsCompanion(outtime: Value(DateTime.now())));
       }
     }
-
     return student;
   }
 
-  Future<List<Student>> fetchOutime() async {
+  Future<List<Student>> fetchOutTime() async {
     return (select(students)..where((tbl) => tbl.outtime.isNotNull())).get();
   }
 
-  Future<List<Student>> fetchIntTime() async {
+  Future<List<Student>> fetchInTime() async {
     return (select(students)..where((tbl) => tbl.intime.isNotNull())).get();
   }
 
   Future<int> deleteAllEntries() {
     return delete(students).go();
   }
+
+  Future<int> deleteRollno(String roll) {
+    return (delete(students)..where((tbl) => tbl.rollno.equals(roll))).go();
+  }
+}
+
+@DriftAccessor(tables: [ArchiveStudents]) // Correct reference to ArchiveStudents
+class ArchiveStudentDao extends DatabaseAccessor<AppDb> with _$ArchiveStudentDaoMixin { // Correct mixin usage
+  final AppDb db;
+  ArchiveStudentDao(this.db) : super(db);
+
+  Future<int> insertArchiveStudent(ArchiveStudentsCompanion studentArchive) => into(archiveStudents).insert(studentArchive);
 }
--- lib/model/database.g.dart
+++ lib/model/database.g.dart
@@ -353,16 +353,333 @@ class StudentsCompanion extends UpdateCompanion<Student> {
   }
 }
 
+class $ArchiveStudentsTable extends ArchiveStudents
+    with TableInfo<$ArchiveStudentsTable, ArchiveStudent> {
+  @override
+  final GeneratedDatabase attachedDatabase;
+  final String? _alias;
+  $ArchiveStudentsTable(this.attachedDatabase, [this._alias]);
+  static const VerificationMeta _rollnoMeta = const VerificationMeta('rollno');
+  @override
+  late final GeneratedColumn<String> rollno = GeneratedColumn<String>(
+      'rollno', aliasedName, false,
+      type: DriftSqlType.string, requiredDuringInsert: true);
+  static const VerificationMeta _nameMeta = const VerificationMeta('name');
+  @override
+  late final GeneratedColumn<String> name = GeneratedColumn<String>(
+      'name', aliasedName, false,
+      type: DriftSqlType.string, requiredDuringInsert: true);
+  static const VerificationMeta _intimeMeta = const VerificationMeta('intime');
+  @override
+  late final GeneratedColumn<DateTime> intime = GeneratedColumn<DateTime>(
+      'intime', aliasedName, true,
+      type: DriftSqlType.dateTime, requiredDuringInsert: false);
+  static const VerificationMeta _outtimeMeta =
+      const VerificationMeta('outtime');
+  @override
+  late final GeneratedColumn<DateTime> outtime = GeneratedColumn<DateTime>(
+      'outtime', aliasedName, true,
+      type: DriftSqlType.dateTime, requiredDuringInsert: false);
+  static const VerificationMeta _departmentMeta =
+      const VerificationMeta('department');
+  @override
+  late final GeneratedColumn<String> department = GeneratedColumn<String>(
+      'department', aliasedName, false,
+      type: DriftSqlType.string, requiredDuringInsert: true);
+  @override
+  List<GeneratedColumn> get $columns =>
+      [rollno, name, intime, outtime, department];
+  @override
+  String get aliasedName => _alias ?? actualTableName;
+  @override
+  String get actualTableName => $name;
+  static const String $name = 'archive_students';
+  @override
+  VerificationContext validateIntegrity(Insertable<ArchiveStudent> instance,
+      {bool isInserting = false}) {
+    final context = VerificationContext();
+    final data = instance.toColumns(true);
+    if (data.containsKey('rollno')) {
+      context.handle(_rollnoMeta,
+          rollno.isAcceptableOrUnknown(data['rollno']!, _rollnoMeta));
+    } else if (isInserting) {
+      context.missing(_rollnoMeta);
+    }
+    if (data.containsKey('name')) {
+      context.handle(
+          _nameMeta, name.isAcceptableOrUnknown(data['name']!, _nameMeta));
+    } else if (isInserting) {
+      context.missing(_nameMeta);
+    }
+    if (data.containsKey('intime')) {
+      context.handle(_intimeMeta,
+          intime.isAcceptableOrUnknown(data['intime']!, _intimeMeta));
+    }
+    if (data.containsKey('outtime')) {
+      context.handle(_outtimeMeta,
+          outtime.isAcceptableOrUnknown(data['outtime']!, _outtimeMeta));
+    }
+    if (data.containsKey('department')) {
+      context.handle(
+          _departmentMeta,
+          department.isAcceptableOrUnknown(
+              data['department']!, _departmentMeta));
+    } else if (isInserting) {
+      context.missing(_departmentMeta);
+    }
+    return context;
+  }
+
+  @override
+  Set<GeneratedColumn> get $primaryKey => const {};
+  @override
+  ArchiveStudent map(Map<String, dynamic> data, {String? tablePrefix}) {
+    final effectivePrefix = tablePrefix != null ? '$tablePrefix.' : '';
+    return ArchiveStudent(
+      rollno: attachedDatabase.typeMapping
+          .read(DriftSqlType.string, data['${effectivePrefix}rollno'])!,
+      name: attachedDatabase.typeMapping
+          .read(DriftSqlType.string, data['${effectivePrefix}name'])!,
+      intime: attachedDatabase.typeMapping
+          .read(DriftSqlType.dateTime, data['${effectivePrefix}intime']),
+      outtime: attachedDatabase.typeMapping
+          .read(DriftSqlType.dateTime, data['${effectivePrefix}outtime']),
+      department: attachedDatabase.typeMapping
+          .read(DriftSqlType.string, data['${effectivePrefix}department'])!,
+    );
+  }
+
+  @override
+  $ArchiveStudentsTable createAlias(String alias) {
+    return $ArchiveStudentsTable(attachedDatabase, alias);
+  }
+}
+
+class ArchiveStudent extends DataClass implements Insertable<ArchiveStudent> {
+  final String rollno;
+  final String name;
+  final DateTime? intime;
+  final DateTime? outtime;
+  final String department;
+  const ArchiveStudent(
+      {required this.rollno,
+      required this.name,
+      this.intime,
+      this.outtime,
+      required this.department});
+  @override
+  Map<String, Expression> toColumns(bool nullToAbsent) {
+    final map = <String, Expression>{};
+    map['rollno'] = Variable<String>(rollno);
+    map['name'] = Variable<String>(name);
+    if (!nullToAbsent || intime != null) {
+      map['intime'] = Variable<DateTime>(intime);
+    }
+    if (!nullToAbsent || outtime != null) {
+      map['outtime'] = Variable<DateTime>(outtime);
+    }
+    map['department'] = Variable<String>(department);
+    return map;
+  }
+
+  ArchiveStudentsCompanion toCompanion(bool nullToAbsent) {
+    return ArchiveStudentsCompanion(
+      rollno: Value(rollno),
+      name: Value(name),
+      intime:
+          intime == null && nullToAbsent ? const Value.absent() : Value(intime),
+      outtime: outtime == null && nullToAbsent
+          ? const Value.absent()
+          : Value(outtime),
+      department: Value(department),
+    );
+  }
+
+  factory ArchiveStudent.fromJson(Map<String, dynamic> json,
+      {ValueSerializer? serializer}) {
+    serializer ??= driftRuntimeOptions.defaultSerializer;
+    return ArchiveStudent(
+      rollno: serializer.fromJson<String>(json['rollno']),
+      name: serializer.fromJson<String>(json['name']),
+      intime: serializer.fromJson<DateTime?>(json['intime']),
+      outtime: serializer.fromJson<DateTime?>(json['outtime']),
+      department: serializer.fromJson<String>(json['department']),
+    );
+  }
+  @override
+  Map<String, dynamic> toJson({ValueSerializer? serializer}) {
+    serializer ??= driftRuntimeOptions.defaultSerializer;
+    return <String, dynamic>{
+      'rollno': serializer.toJson<String>(rollno),
+      'name': serializer.toJson<String>(name),
+      'intime': serializer.toJson<DateTime?>(intime),
+      'outtime': serializer.toJson<DateTime?>(outtime),
+      'department': serializer.toJson<String>(department),
+    };
+  }
+
+  ArchiveStudent copyWith(
+          {String? rollno,
+          String? name,
+          Value<DateTime?> intime = const Value.absent(),
+          Value<DateTime?> outtime = const Value.absent(),
+          String? department}) =>
+      ArchiveStudent(
+        rollno: rollno ?? this.rollno,
+        name: name ?? this.name,
+        intime: intime.present ? intime.value : this.intime,
+        outtime: outtime.present ? outtime.value : this.outtime,
+        department: department ?? this.department,
+      );
+  ArchiveStudent copyWithCompanion(ArchiveStudentsCompanion data) {
+    return ArchiveStudent(
+      rollno: data.rollno.present ? data.rollno.value : this.rollno,
+      name: data.name.present ? data.name.value : this.name,
+      intime: data.intime.present ? data.intime.value : this.intime,
+      outtime: data.outtime.present ? data.outtime.value : this.outtime,
+      department:
+          data.department.present ? data.department.value : this.department,
+    );
+  }
+
+  @override
+  String toString() {
+    return (StringBuffer('ArchiveStudent(')
+          ..write('rollno: $rollno, ')
+          ..write('name: $name, ')
+          ..write('intime: $intime, ')
+          ..write('outtime: $outtime, ')
+          ..write('department: $department')
+          ..write(')'))
+        .toString();
+  }
+
+  @override
+  int get hashCode => Object.hash(rollno, name, intime, outtime, department);
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is ArchiveStudent &&
+          other.rollno == this.rollno &&
+          other.name == this.name &&
+          other.intime == this.intime &&
+          other.outtime == this.outtime &&
+          other.department == this.department);
+}
+
+class ArchiveStudentsCompanion extends UpdateCompanion<ArchiveStudent> {
+  final Value<String> rollno;
+  final Value<String> name;
+  final Value<DateTime?> intime;
+  final Value<DateTime?> outtime;
+  final Value<String> department;
+  final Value<int> rowid;
+  const ArchiveStudentsCompanion({
+    this.rollno = const Value.absent(),
+    this.name = const Value.absent(),
+    this.intime = const Value.absent(),
+    this.outtime = const Value.absent(),
+    this.department = const Value.absent(),
+    this.rowid = const Value.absent(),
+  });
+  ArchiveStudentsCompanion.insert({
+    required String rollno,
+    required String name,
+    this.intime = const Value.absent(),
+    this.outtime = const Value.absent(),
+    required String department,
+    this.rowid = const Value.absent(),
+  })  : rollno = Value(rollno),
+        name = Value(name),
+        department = Value(department);
+  static Insertable<ArchiveStudent> custom({
+    Expression<String>? rollno,
+    Expression<String>? name,
+    Expression<DateTime>? intime,
+    Expression<DateTime>? outtime,
+    Expression<String>? department,
+    Expression<int>? rowid,
+  }) {
+    return RawValuesInsertable({
+      if (rollno != null) 'rollno': rollno,
+      if (name != null) 'name': name,
+      if (intime != null) 'intime': intime,
+      if (outtime != null) 'outtime': outtime,
+      if (department != null) 'department': department,
+      if (rowid != null) 'rowid': rowid,
+    });
+  }
+
+  ArchiveStudentsCompanion copyWith(
+      {Value<String>? rollno,
+      Value<String>? name,
+      Value<DateTime?>? intime,
+      Value<DateTime?>? outtime,
+      Value<String>? department,
+      Value<int>? rowid}) {
+    return ArchiveStudentsCompanion(
+      rollno: rollno ?? this.rollno,
+      name: name ?? this.name,
+      intime: intime ?? this.intime,
+      outtime: outtime ?? this.outtime,
+      department: department ?? this.department,
+      rowid: rowid ?? this.rowid,
+    );
+  }
+
+  @override
+  Map<String, Expression> toColumns(bool nullToAbsent) {
+    final map = <String, Expression>{};
+    if (rollno.present) {
+      map['rollno'] = Variable<String>(rollno.value);
+    }
+    if (name.present) {
+      map['name'] = Variable<String>(name.value);
+    }
+    if (intime.present) {
+      map['intime'] = Variable<DateTime>(intime.value);
+    }
+    if (outtime.present) {
+      map['outtime'] = Variable<DateTime>(outtime.value);
+    }
+    if (department.present) {
+      map['department'] = Variable<String>(department.value);
+    }
+    if (rowid.present) {
+      map['rowid'] = Variable<int>(rowid.value);
+    }
+    return map;
+  }
+
+  @override
+  String toString() {
+    return (StringBuffer('ArchiveStudentsCompanion(')
+          ..write('rollno: $rollno, ')
+          ..write('name: $name, ')
+          ..write('intime: $intime, ')
+          ..write('outtime: $outtime, ')
+          ..write('department: $department, ')
+          ..write('rowid: $rowid')
+          ..write(')'))
+        .toString();
+  }
+}
+
 abstract class _$AppDb extends GeneratedDatabase {
   _$AppDb(QueryExecutor e) : super(e);
   $AppDbManager get managers => $AppDbManager(this);
   late final $StudentsTable students = $StudentsTable(this);
+  late final $ArchiveStudentsTable archiveStudents =
+      $ArchiveStudentsTable(this);
   late final StudentDao studentDao = StudentDao(this as AppDb);
+  late final ArchiveStudentDao archiveStudentDao =
+      ArchiveStudentDao(this as AppDb);
   @override
   Iterable<TableInfo<Table, Object?>> get allTables =>
       allSchemaEntities.whereType<TableInfo<Table, Object?>>();
   @override
-  List<DatabaseSchemaEntity> get allSchemaEntities => [students];
+  List<DatabaseSchemaEntity> get allSchemaEntities =>
+      [students, archiveStudents];
 }
 
 typedef $$StudentsTableCreateCompanionBuilder = StudentsCompanion Function({
@@ -507,13 +824,146 @@ class $$StudentsTableOrderingComposer
           ColumnOrderings(column, joinBuilders: joinBuilders));
 }
 
+typedef $$ArchiveStudentsTableCreateCompanionBuilder = ArchiveStudentsCompanion
+    Function({
+  required String rollno,
+  required String name,
+  Value<DateTime?> intime,
+  Value<DateTime?> outtime,
+  required String department,
+  Value<int> rowid,
+});
+typedef $$ArchiveStudentsTableUpdateCompanionBuilder = ArchiveStudentsCompanion
+    Function({
+  Value<String> rollno,
+  Value<String> name,
+  Value<DateTime?> intime,
+  Value<DateTime?> outtime,
+  Value<String> department,
+  Value<int> rowid,
+});
+
+class $$ArchiveStudentsTableTableManager extends RootTableManager<
+    _$AppDb,
+    $ArchiveStudentsTable,
+    ArchiveStudent,
+    $$ArchiveStudentsTableFilterComposer,
+    $$ArchiveStudentsTableOrderingComposer,
+    $$ArchiveStudentsTableCreateCompanionBuilder,
+    $$ArchiveStudentsTableUpdateCompanionBuilder> {
+  $$ArchiveStudentsTableTableManager(_$AppDb db, $ArchiveStudentsTable table)
+      : super(TableManagerState(
+          db: db,
+          table: table,
+          filteringComposer:
+              $$ArchiveStudentsTableFilterComposer(ComposerState(db, table)),
+          orderingComposer:
+              $$ArchiveStudentsTableOrderingComposer(ComposerState(db, table)),
+          updateCompanionCallback: ({
+            Value<String> rollno = const Value.absent(),
+            Value<String> name = const Value.absent(),
+            Value<DateTime?> intime = const Value.absent(),
+            Value<DateTime?> outtime = const Value.absent(),
+            Value<String> department = const Value.absent(),
+            Value<int> rowid = const Value.absent(),
+          }) =>
+              ArchiveStudentsCompanion(
+            rollno: rollno,
+            name: name,
+            intime: intime,
+            outtime: outtime,
+            department: department,
+            rowid: rowid,
+          ),
+          createCompanionCallback: ({
+            required String rollno,
+            required String name,
+            Value<DateTime?> intime = const Value.absent(),
+            Value<DateTime?> outtime = const Value.absent(),
+            required String department,
+            Value<int> rowid = const Value.absent(),
+          }) =>
+              ArchiveStudentsCompanion.insert(
+            rollno: rollno,
+            name: name,
+            intime: intime,
+            outtime: outtime,
+            department: department,
+            rowid: rowid,
+          ),
+        ));
+}
+
+class $$ArchiveStudentsTableFilterComposer
+    extends FilterComposer<_$AppDb, $ArchiveStudentsTable> {
+  $$ArchiveStudentsTableFilterComposer(super.$state);
+  ColumnFilters<String> get rollno => $state.composableBuilder(
+      column: $state.table.rollno,
+      builder: (column, joinBuilders) =>
+          ColumnFilters(column, joinBuilders: joinBuilders));
+
+  ColumnFilters<String> get name => $state.composableBuilder(
+      column: $state.table.name,
+      builder: (column, joinBuilders) =>
+          ColumnFilters(column, joinBuilders: joinBuilders));
+
+  ColumnFilters<DateTime> get intime => $state.composableBuilder(
+      column: $state.table.intime,
+      builder: (column, joinBuilders) =>
+          ColumnFilters(column, joinBuilders: joinBuilders));
+
+  ColumnFilters<DateTime> get outtime => $state.composableBuilder(
+      column: $state.table.outtime,
+      builder: (column, joinBuilders) =>
+          ColumnFilters(column, joinBuilders: joinBuilders));
+
+  ColumnFilters<String> get department => $state.composableBuilder(
+      column: $state.table.department,
+      builder: (column, joinBuilders) =>
+          ColumnFilters(column, joinBuilders: joinBuilders));
+}
+
+class $$ArchiveStudentsTableOrderingComposer
+    extends OrderingComposer<_$AppDb, $ArchiveStudentsTable> {
+  $$ArchiveStudentsTableOrderingComposer(super.$state);
+  ColumnOrderings<String> get rollno => $state.composableBuilder(
+      column: $state.table.rollno,
+      builder: (column, joinBuilders) =>
+          ColumnOrderings(column, joinBuilders: joinBuilders));
+
+  ColumnOrderings<String> get name => $state.composableBuilder(
+      column: $state.table.name,
+      builder: (column, joinBuilders) =>
+          ColumnOrderings(column, joinBuilders: joinBuilders));
+
+  ColumnOrderings<DateTime> get intime => $state.composableBuilder(
+      column: $state.table.intime,
+      builder: (column, joinBuilders) =>
+          ColumnOrderings(column, joinBuilders: joinBuilders));
+
+  ColumnOrderings<DateTime> get outtime => $state.composableBuilder(
+      column: $state.table.outtime,
+      builder: (column, joinBuilders) =>
+          ColumnOrderings(column, joinBuilders: joinBuilders));
+
+  ColumnOrderings<String> get department => $state.composableBuilder(
+      column: $state.table.department,
+      builder: (column, joinBuilders) =>
+          ColumnOrderings(column, joinBuilders: joinBuilders));
+}
+
 class $AppDbManager {
   final _$AppDb _db;
   $AppDbManager(this._db);
   $$StudentsTableTableManager get students =>
       $$StudentsTableTableManager(_db, _db.students);
+  $$ArchiveStudentsTableTableManager get archiveStudents =>
+      $$ArchiveStudentsTableTableManager(_db, _db.archiveStudents);
 }
 
 mixin _$StudentDaoMixin on DatabaseAccessor<AppDb> {
   $StudentsTable get students => attachedDatabase.students;
+}
+mixin _$ArchiveStudentDaoMixin on DatabaseAccessor<AppDb> {
+  $ArchiveStudentsTable get archiveStudents => attachedDatabase.archiveStudents;
 }
