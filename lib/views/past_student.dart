91540183cbd4fd7d1e05370cdb6a0d690e482b34
satyam_45 <satyampokharna45@gmail.com>
2024-08-12 15:01:35 +0530

    past_student: Add screen to show students from ArchiveStudent
    Fixes #49

---
+++ lib/views/past_student.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:library_qr/model/database.dart';
+
+class PastStudent extends StatefulWidget {
+  const PastStudent({super.key});
+  @override
+  PastStudentState createState() => PastStudentState();
+}
+
+class PastStudentState extends State<PastStudent> {
+  late Future<List<ArchiveStudent>> paststudent;
+  int count = 0;
+  late AppDb db;
+
+  @override
+  void initState() {
+    super.initState();
+    db = AppDb.instance;
+    fetchstudent();  // Initialize fetching students
+  }
+
+  Future<void> fetchstudent() async {
+    final pastList = await db.archiveStudentDao.fetchStudents();
+    setState(() {
+      paststudent = Future.value(pastList);
+      count = pastList.length;
+    });
+  }
+
+  void _deleteAllEntries() async {
+    await db.archiveStudentDao.deleteall();
+    fetchstudent();  // Refresh the list after deletion
+  }
+
+  @override
+  void dispose() {
+    // Clean up resources here if necessary
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Past Students'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.delete),
+            onPressed: _deleteAllEntries,
+          ),
+        ],
+      ),
+      body: FutureBuilder<List<ArchiveStudent>>(
+        future: paststudent,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return const Center(child: Text('No Student'),);
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data?.length ?? 0,
+              itemBuilder: (context, index) {
+                final archiveStudent = snapshot.data![index];
+                return Card(
+                  child: ListTile(
+                    title: Text(archiveStudent.name ),
+                    subtitle: Text('Roll No: ${archiveStudent.rollno}\nIntime: ${archiveStudent.intime}\nOuttime: ${archiveStudent.outtime}\nDep: ${archiveStudent.department}'),
+                  ));
+              },
+            );
+          }
+        },
+      ));
+  }
+}
